4a5ef5afa2cad2639b48a487286d037f34ea328c
Your Name <you@example.com>
2024-03-05 02:13:38 +0000

    programa c funcion printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Punto de Entrada
+ *
+ *Return: Siempre 0 (finalizado)
+ *
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
