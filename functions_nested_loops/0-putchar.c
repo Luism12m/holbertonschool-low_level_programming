cbbc2099047a514351cf939db3e103ee0bf93be0
Your Name <you@example.com>
2024-04-22 03:35:58 +0000

    funciones

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,27 +1,27 @@
 /*
- * File: 0-putchar.c
- * Auth: Luis Zipa
- *
- */
+* File: 0-putchar.c
+* Auth: Luis Zipa
+*
+*/
 
 #include "main.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
+* main - Prints "Holberton" followed by a new line.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 _putchar('_');
 _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-  return (0);
+return (0);
 }
