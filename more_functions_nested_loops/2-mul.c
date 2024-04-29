c7866239d89d38e7c02904e32d9b1a833f712275
Tu Nombre <you@example.com>
2024-04-29 15:22:34 -0500

    multiplicar 2 enteros

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplicar dos enteros
+ * @a: primer entero
+ * @b: segundo entero
+ *
+ * Return: producto de los dos
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
