f62e12d2d2e0dac56ed2068c8ffe0580afdc4466
Tu Nombre <you@example.com>
2024-05-04 20:24:58 -0500

    copiar el apuntamiento de una cadena

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,21 +1,23 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _strcpy - Copies string pointed to by src
- * @dest: Array being copied to
- * @src: String to be copied
- *
- * Return: The value of pointer to dest
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte, to the
+ * buffer pointed to by dest.
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len;
-	int i = 0;
+	int count = 0;
 
-	while (*src++)
-		i++;
-	len = i;
-	for (i = 0; i <= len; i++)
-		dest[i] = src[i];
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
 	return (dest);
 }
