e6442f07b7348732c98a0a7490e5112049036aa2
Your Name <you@example.com>
2024-05-17 00:31:58 +0000

    funcion que localice una subcadena

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -5,7 +5,7 @@
 * main - check the code
 *
 * Return: Always 0.
-*/
+
 int main(void)
 {
 char str[] = "Look up!\n";
@@ -14,5 +14,22 @@ char *ptr;
 ptr = string_toupper(str);
 printf("%s", ptr);
 printf("%s", str);
+return (0);
+}
+*/
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char *s = "hello, world";
+char *f = "world";
+char *t;
+
+t = _strstr(s, f);
+printf("%s\n", t);
 return (0);
 }
