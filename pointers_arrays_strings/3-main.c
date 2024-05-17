42e566a8ffd6351adf5cd40e1285eba383dd1cd5
Your Name <you@example.com>
2024-05-17 00:03:47 +0000

    obtener longitud de una sucadena de prefijo

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -5,7 +5,7 @@
 * main - check the code
 *
 * Return: Always 0.
-*/
+
 int main(void)
 {
 char s1[] = "Hello";
@@ -14,5 +14,22 @@ char s2[] = "World!";
 printf("%d\n", _strcmp(s1, s2));
 printf("%d\n", _strcmp(s2, s1));
 printf("%d\n", _strcmp(s1, s1));
+return (0);
+}
+*/
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char *s = "hello, world";
+char *f = "oleh";
+unsigned int n;
+
+n = _strspn(s, f);
+printf("%u\n", n);
 return (0);
 }
