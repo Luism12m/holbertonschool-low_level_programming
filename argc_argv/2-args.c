b2b7cfde9ac616fb244ce809ce730dbceaa55670
Your Name <you@example.com>
2024-05-25 02:36:12 +0000

    imprimir argumento por linea

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -5,11 +5,11 @@
 * @argv: array that contains the program command line arguments.
 * Return: 0 - success.
 */
-
 int main(int argc, char *argv[])
 {
-int = i;
-for (int i = 0; i < argc; i++)
-printf(" argumento %d, %s \n ", i, argv[i]);
-return  (0);
+int i;
+
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
 }
