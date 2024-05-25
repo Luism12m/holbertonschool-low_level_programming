f63d40a8f4a50ee5faa3d6fa0c2c8a13de755dac
Your Name <you@example.com>
2024-05-25 01:50:24 +0000

    numero de argumentos

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
 * main - prints the number of arguments passed into it.
@@ -7,6 +8,6 @@
 */
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-printf("%d\n",argc -1);
+printf("%d\n", argc - 1);
 return (0);
 }
