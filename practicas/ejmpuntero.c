e0d2d1ba78e24bded4a9681151bece150dcf7499
Your Name <you@example.com>
2024-06-19 01:21:02 +0000

    ejemplo puntero

--- practicas/ejmpuntero.c
+++ practicas/ejmpuntero.c
@@ -3,8 +3,11 @@
 
 int main(){
   int x=38;
+  /*en la siguiente linea capturo la direccion de memoria que obtengo con: &x en la linea 8*/
+  int* dirx=&x;
   printf("el valor de x es : %d\n",x);
   printf("la direccion de memoria de x es : %ls\n",&x);
+  printf("la direccion del puntero de int* es : %ls\n",dirx);
 
 
 
