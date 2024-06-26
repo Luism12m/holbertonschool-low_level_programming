455b299ef0ecc9e033d0917e56600231d04ddab3
Your Name <you@example.com>
2024-06-26 20:03:20 +0000

    ejm printf

--- practicas/ejmprintf.c
+++ practicas/ejmprintf.c
@@ -2,10 +2,25 @@
 
 int main()
 {
+  /*usando banderas (espacios,mas,menos)*/
   int age;
   age = 25;
-  printf ( "I am %d years old\n",age );
+  printf ( "I am %+05d years old\n",age );
+  
 
+  /*imprimir varios argumentos o variables*/
   int x = 5, y = 10;
   printf("x es %dy y es %d\n", x, y);
+
+
+  /*La opcion %s nos permite imprimir una cadena dentro de una cadena*/
+  int year =11;
+  char *grade;
+  if (year == 11 ) grade = "junior";
+  printf ( "%s is a %s\n", "Fred", grade );
+  
+
+ 
+    
+  
 }
