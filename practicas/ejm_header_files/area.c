d852fd32b81c4e775042ceac28132922baf93266
Your Name <you@example.com>
2024-06-26 22:24:40 +0000

    ejemplo header file

---
+++ practicas/ejm_header_files/area.c
@@ -0,0 +1,15 @@
+//area.c
+
+#include "main.h"
+#define PI 3.14159
+
+// Implementacinnn de la funcion para calcular el area de un circulo
+
+float calcularAreaCirculo(float radio) {
+  return PI * radio * radio;
+}
+
+// Implementacinnn de la funcion para calcular el area d eun rectangulo
+float calcularAreaRectangulo(float base, float altura) {
+  return base * altura;
+}
