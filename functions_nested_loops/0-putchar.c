4ac53990a235b17603023e5afc4c47fff5ff3058
Luis Zipa <you@example.com>
2024-08-14 19:26:46 +0000

    estilo betty

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include "main.h"
+/**
+ * main - 
+ * @char: The pointer to print
+ * @i: iteration variable
+ *
+ * Return: always 0.
+ */
 
 int main(void){
   char * text = "_putchar\n";
   int i = 0;
-  for (i = 0; text[i]; i++){
+  for (i = 0; text[i]; i++)
+    {
     _putchar(text[i]);
 
-  }
+    }
   return (0);
 }
