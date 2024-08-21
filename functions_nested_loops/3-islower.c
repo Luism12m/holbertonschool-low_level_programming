8da05c99c984f70dae4871d6bd13a9db88c27afe
Luis Zipa <you@example.com>
2024-08-21 22:19:30 +0000

    lowercase character betty style

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
- * _islower - entry point
- *
+ * _islower - Check if a character is a lowercase letter.
+ * @c: The character to be checked.
  * Description: This function prints checks for lowercase character
  *
- * Return: 0 lowercase
- * Return: 1 otherwise
+ * Return: 0 lowercase , Return: 1 otherwise
 */
 
 
 int _islower(int c)
 {
-  
-    if (c >= 97 && c <= 122)
-    {
-      return (1);
-    }
-    return (0);
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
 }
