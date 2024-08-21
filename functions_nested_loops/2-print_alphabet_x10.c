f44df457878de9b8a21246d55c61204074add8a9
Luis Zipa <you@example.com>
2024-08-21 21:10:46 +0000

    betty style

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,22 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: This function prints the alphabet ten times
+ *
+ * Return: void
+*/
 
 void print_alphabet_x10(void)
 {
-  int i = 0;
-  int j = 0;
-  for(i = 0; i < 10; i++)
-    {
-      for(j = 0; j < 26; j++)
-	{
-	  _putchar(97 + j);
-	}
-      _putchar('\n');
-    }
+int i = 0;
+int j = 0;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 26; j++)
+{
+_putchar(97 + j);
+}
+_putchar('\n');
+}
 }
