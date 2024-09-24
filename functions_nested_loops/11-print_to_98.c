cbd88f9b9c7ff5e40c9f7c0a160292de410a90d6
luis zipa <you@example.com>
2024-09-24 00:59:34 +0000

    imprimir numeros naturales

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * void print_to_98(int n) -print natural numbers.
+ * Description: This function prints natural numbers from n to 98.
+ *@n : number to start.
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
