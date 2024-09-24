50ab4176ace092b13dfb6678baa484dfeff76b50
luis zipa <you@example.com>
2024-09-24 00:15:52 +0000

    tabla 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,31 +1,26 @@
 #include "main.h"
 /**
- * _times_table -nine table.
- * 
+ * times_table -print nine table.
  * Description: This function prints nine table
- *
- * 
  */
 
 void times_table(void)
 {
-	int number, multiplication,product ;
-
-	for (number = 0; number <= 9; number++)
-    {
-      _putchar('0');
-  for (multiplication = 1; multiplication <= 9; multiplication++)
-  {
-	  _putchar(',');
-	  _putchar(' ');
-	  product = number * multiplication;
-	  if (product <= 9)
-		  _putchar(' ');
-	  else
-		  _putchar((product / 10) + '0');
-
-	  _putchar((product % 10) + '0');
-  }
-  _putchar('\n');
-    }
+int number, multiplication, product;
+for (number = 0; number <= 9; number++)
+{
+_putchar('0');
+for (multiplication = 1; multiplication <= 9; multiplication++)
+{
+_putchar(',');
+_putchar(' ');
+product = number * multiplication;
+if (product <= 9)
+_putchar(' ');
+else
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+_putchar('\n');
+}
 }
