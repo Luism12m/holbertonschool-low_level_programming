fab6e1c03eb72aff2e3641a234e70309b2ad7952
Luis Zipa <you@example.com>
2024-10-15 02:01:35 +0000

    ciclos for y  while

---
+++ practicas/estructuras_basicas/ciclos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - entry poit of the program
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+int j = 0;
+for (i = 0; i < 5; i++)
+{
+printf("Numero for : %d\n", i);
+}
+while (j < 5)
+{
+printf("Numero while :%d\n", j);
+j++;
+}
+return (0);
+}
