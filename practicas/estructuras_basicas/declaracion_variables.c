c4b1b8c7d56db6b37082272521fd15f6bbc6a966
Luis Zipa <you@example.com>
2024-10-15 00:13:06 +0000

    declaracion de variables

---
+++ practicas/estructuras_basicas/declaracion_variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ *Return: always 0 (success)
+ */
+
+int main (void)
+{
+  int a = 5;
+  float b = 3.14;
+  char c = 'Z';
+
+  printf("Entero : %d, Flotante: %.3f, Caracter: %c\n", a,b,c);
+  return 0;
+}
