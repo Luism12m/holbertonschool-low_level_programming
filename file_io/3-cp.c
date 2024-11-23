a4244c44ca654be73b17e32ba181dba9546ec472
luis zipa <you@example.com>
2024-11-23 23:42:05 +0000

    betty cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,56 +21,55 @@ char buffer[1024];
 /* Verifica el numero de argumentos */
 if (argc != 3)
 {
-      dprintf(2, "Uso: cp file_from file_to\n");
-      exit(97);
-    }
+dprintf(2, "Uso: cp file_from file_to\n");
+exit(97);
+}
 
-  /* Abre el archivo de origen */
-  fd_from = open(argv[1], O_RDONLY);
-  if (fd_from == -1)
-    {
-      dprintf(2, "Error: No se puede leer el archivo %s\n", argv[1]);
-      exit(98);
-    }
+/* Abre el archivo de origen */
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(2, "Error: No se puede leer el archivo %s\n", argv[1]);
+exit(98);
+}
 
-  /* Abre el archivo de destino */
-  fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-  if (fd_to == -1)
-    {
-      dprintf(2, "Error: No se puede escribir en el archivo %s\n", argv[2]);
-      exit(99);
-    }
+/* Abre el archivo de destino */
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+dprintf(2, "Error: No se puede escribir en el archivo %s\n", argv[2]);
+exit(99);
+}
 
-  /* Copia el contenido de file_from a file_to */
-  while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
-    {
-      bytes_written = write(fd_to, buffer, bytes_read);
-      if (bytes_written == -1)
-	{
-	  dprintf(2, "Error: No se puede escribir en el archivo %s\n", argv[2]);
-	  exit(99);
-	}
-    }
+/* Copia el contenido de file_from a file_to */
+while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+{
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1)
+{
+dprintf(2, "Error: No se puede escribir en el archivo %s\n", argv[2]);
+exit(99);
+}
+}
 
-  /* Verifica si hubo error en la lectura */
-  if (bytes_read == -1)
-    {
-      dprintf(2, "Error: No se puede leer el archivo %s\n", argv[1]);
-      exit(98);
-    }
+/* Verifica si hubo error en la lectura */
+if (bytes_read == -1)
+{
+dprintf(2, "Error: No se puede leer el archivo %s\n", argv[1]);
+exit(98);
+}
 
-  /* Cierra los descriptores de archivo */
-  if (close(fd_from) == -1)
-    {
-      dprintf(2, "Error: No se puede cerrar el descriptor de archivo %d\n", fd_from);
-      exit(100);
-    }
+/* Cierra los descriptores de archivo */
+if (close(fd_from) == -1)
+{
+dprintf(2, "Error: No se puede cerrar el descriptor de archivo %d\n", fd_from);
+exit(100);
+}
 
-  if (close(fd_to) == -1)
-    {
-      dprintf(2, "Error: No se puede cerrar el descriptor de archivo %d\n", fd_to);
-      exit(100);
-    }
-
-  return (0);
+if (close(fd_to) == -1)
+{
+dprintf(2, "Error: No se puede cerrar el descriptor de archivo %d\n", fd_to);
+exit(100);
+}
+return (0);
 }
