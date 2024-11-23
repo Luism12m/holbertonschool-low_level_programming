d7658f19057bbaeaf6ab32104317f3780fe48b34
luis zipa <you@example.com>
2024-11-23 21:56:47 +0000

    betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,42 +9,44 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd; 
-  char *buffer;  
-  ssize_t bytes_read, bytes_written;
-  if (filename == NULL)
-    return (0);
+int fd;
+char *buffer;
+ssize_t bytes_read, bytes_written;
+if (filename == NULL)
+return (0);
 
-  fd = open(filename, O_RDONLY);
-  if (fd == -1) {
-    perror("Error opening file");
-    return (0);
-  }
-  
-  buffer = malloc(sizeof(char) * letters);
-  if (buffer == NULL) {
-    perror("Memory allocation failed");
-    close(fd);
-    return (0);
-  }
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+perror("Error opening file");
+return (0);
+}
 
-  bytes_read = read(fd, buffer, letters);
-  if (bytes_read == -1) {
-    perror("Error reading file");
-    free(buffer);
-    close(fd);
-    return (0);
-  }
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+perror("Memory allocation failed");
+close(fd);
+return (0);
+}
 
-  bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-  if (bytes_written == -1 || bytes_written != bytes_read) {
-    perror("Error writing to stdout");
-    free(buffer);
-    close(fd);
-    return (0);
-  }
-  free(buffer);
-  close(fd);
-
-  return (bytes_read);
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+perror("Error reading file");
+free(buffer);
+close(fd);
+return (0);
+}
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+perror("Error writing to stdout");
+free(buffer);
+close(fd);
+return (0);
+}
+free(buffer);
+close(fd);
+return (bytes_read);
 }
